773925c63e31ddf6958cf769c1a56777f70193a8
Ahmad Abu Ghali <38799135+AhmadMAbuGhali@users.noreply.github.com>
2023-09-02 20:14:58 +0300

    Initial commit

---
+++ lib/resources/assets_manager.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
+const String IMAGE_PATH = 'assets/images';
+
+class ImageAssets {
+  static const String logopng = '$IMAGE_PATH/logo.png';
+
+}
+
+const String ICON_PATH = 'assets/icon';
+
+class IconAssets {
+
+  static const String alert = '$ICON_PATH/alert.svg';
+}
